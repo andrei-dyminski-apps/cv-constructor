35e276721d00592970dc03596cc70783627baf8f
Andrei Dyminski <smartfox87@gmail.com>
2025-03-22 21:44:49 +0100

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,22 +4,22 @@ import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
+export default defineConfig([
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   eslintConfigPrettier,
   eslintPluginPrettierRecommended,
+  globalIgnores(['.react-router/']),
   {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: { globals: globals.browser },
     rules: {
       'react/react-in-jsx-scope': 'off',
       '@typescript-eslint/no-namespace': 'off',
-      '@typescript-eslint/no-empty-object-type': 'off',
     },
-    ignores: ['.idea/', '.react-router/'],
   },
-];
+]);
