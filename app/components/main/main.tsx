29197e8d305ade09c6438caa145bca6f609d4ef9
Andrei Dyminski <smartfox87@gmail.com>
2025-03-28 16:17:28 +0100

    fix print version

--- app/components/main/main.tsx
+++ app/components/main/main.tsx
@@ -3,14 +3,20 @@ import { useData } from '~/hooks/data';
 import { AboutMe } from '~/components/main/about-me';
 
 export const Main = () => {
-  const { data } = useData();
+  const { data, coreSkills } = useData();
   return (
     <div className="relative flex flex-col gap-1 px-5 py-4">
       <h1 className="text-2xl leading-none font-bold uppercase">
         {data.personal.name}
       </h1>
       <ExperienceList />
-      <div className="absolute top-[300mm] left-0 w-full px-5">
+      <div
+        className={
+          Object.values(coreSkills).filter(Boolean).length < 3
+            ? 'absolute top-[300mm] left-0 w-full px-5'
+            : ''
+        }
+      >
         <AboutMe />
       </div>
     </div>
